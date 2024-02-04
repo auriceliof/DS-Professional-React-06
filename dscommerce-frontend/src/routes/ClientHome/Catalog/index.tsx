6f47396b7a04603ba0ab2b42c8f8205fb4cd4a60
auriceliof <auricelio.suporte@hotmail.com>
2024-02-04 20:40:27 -0300

    06-09 Buscando a próxima página de produtos

--- dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -13,6 +13,8 @@ type QueryParams = {
 
 export default function Catalog() {
 
+  const [isLastPage, setIsLastPage] = useState(false);
+
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
   const [queryParams, setQueryParams] = useState<QueryParams>({
@@ -24,13 +26,20 @@ export default function Catalog() {
 
     productService.findPageRequest(queryParams.page, queryParams.name)
       .then(response => {
-        setProducts(response.data.content);
+        const nextPage = response.data.content;
+        setProducts(products.concat(nextPage));
+        setIsLastPage(response.data.last);
       });
 
   }, [queryParams]);
 
   function handleSearch(searchText: string) {
-    setQueryParams({ ...queryParams, name: searchText })
+    setProducts([])
+    setQueryParams({ ...queryParams, page: 0, name: searchText })
+  }
+
+  function handleNextPageClick() {
+    setQueryParams({ ...queryParams, page: queryParams.page + 1})
   }
 
   return (
@@ -46,9 +55,12 @@ export default function Catalog() {
             )
           }          
         </div>
-
-        <ButtonNextPage />
-
+        {
+          !isLastPage &&
+          <div onClick={handleNextPageClick}>
+            <ButtonNextPage />
+          </div>
+        }
       </section>
     </main>
   );
