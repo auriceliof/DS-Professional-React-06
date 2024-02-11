e6b1c3ef7cdef98fbb648ff9b75829181d40971b
auriceliof <auricelio.suporte@hotmail.com>
2024-02-11 12:51:01 -0300

    02 - Header and Basic layout structure

--- DESAFIO/desafio-dsfilter/src/App.tsx
+++ DESAFIO/desafio-dsfilter/src/App.tsx
@@ -1,8 +1,10 @@
-import "./App.css";
+import "./index.css";
+import Header from "./components/Header/Index";
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>DESAFIO DSFILTER</h1>
     </div>
   );
---
+++ DESAFIO/desafio-dsfilter/src/components/Header/Index.tsx
@@ -0,0 +1,16 @@
+import './styles.css';
+
+export default function Header() {
+  return (
+    <header className="dsf-header">
+      <nav className="dsf-container">
+        <div>
+          <h1>DSFilter</h1>
+        </div>
+        <div>
+            <p>6 produto(s)</p>
+        </div>
+      </nav>
+    </header>
+  );
+}
